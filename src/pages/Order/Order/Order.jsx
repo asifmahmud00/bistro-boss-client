d7b194bb41bdc769dcfc05b347beb153cc224da5
asifmahmud00 <asif.mahmud00@gmail.com>
2023-08-23 08:28:19 +0600

    4

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,12 @@
+import orderCover from '../../../assets/shop/banner2.jpg';
+import Cover from '../../Shared/Cover/Cover';
+
+const Order = () => {
+    return (
+        <div>
+            <Cover img={orderCover} title="Order Food"></Cover>
+        </div>
+    );
+};
+
+export default Order;
