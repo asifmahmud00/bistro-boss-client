786314e110a685ad2774a3c2447a4737e3e2db75
asifmahmud00 <asif.mahmud00@gmail.com>
2023-08-24 23:24:33 +0600

    5th

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,10 +1,32 @@
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import orderCover from '../../../assets/shop/banner2.jpg';
 import Cover from '../../Shared/Cover/Cover';
+import { useState } from 'react';
+import useMenu from '../../../hooks/useMenu';
 
 const Order = () => {
+    const [tabIndex,setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const offered = menu.filter(item => item.category === 'offered');
+    const pizza = menu.filter(item => item.category === 'pizza');
     return (
         <div>
             <Cover img={orderCover} title="Order Food"></Cover>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => console.log(index)}>
+                <TabList>
+                    <Tab>SALAD</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
