7fe9edc77e6d2a0a8453a26a66e32d8f93126526
asifmahmud00 <asif.mahmud00@gmail.com>
2023-09-10 06:59:47 +0600

    17

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
@@ -16,10 +17,16 @@ const Navbar = () => {
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
         <li><Link to="/secret">Secret</Link></li>
+        <li><Link to="/">
+            <button className="btn">
+                <FaShoppingCart></FaShoppingCart>
+                <div className="badge badge-secondary">+0</div>
+            </button>
+        </Link></li>
 
         {
             user ? <>
-            <span>{user?.displayName}</span>
+                <span>{user?.displayName}</span>
                 <button onClick={handleLogout} className="btn btn-ghost">Logout</button>
 
             </> : <>
