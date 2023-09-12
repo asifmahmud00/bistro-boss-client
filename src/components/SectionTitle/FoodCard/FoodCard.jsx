4a00102156c751770eed89fed41b4fb8ad321e2b
asifmahmud00 <asif.mahmud00@gmail.com>
2023-09-12 19:17:35 +0600

    18

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -1,9 +1,44 @@
-
+import { useContext } from "react";
+import {AuthContext} from "../../../provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
  
 const FoodCard = ({item}) => {
     const { name, image, price, recipe } = item;
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+
     const handleAddToCart = item =>{
         console.log(item);
+        if(user){
+            fetch('http://localhost:5000/carts')
+            .then(res => res.json())
+            .then(data => {
+                if(data.insertedId){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Item Added to cart Successfully',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'To order the food, Please Login',               
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login now!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login');
+                }
+              })
+        }
     }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
