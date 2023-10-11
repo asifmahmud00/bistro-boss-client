32086466e98bf6d23ae8a8a98617d2c297b4ea95
asifmahmud00 <asif.mahmud00@gmail.com>
2023-10-11 07:30:57 +0600

    11102023-webph- project completed

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -65,7 +65,7 @@ const SignUp = () => {
                         <h1 className="text-5xl font-bold">Sign up now!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                             <div className="form-control">
                                 <label className="label">
